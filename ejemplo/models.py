4e3fda156fe6f1fbeee9abc72ce1e8a85c9550a4
Lorenzo <lorenzo.macintyre@id90travel.com>
2022-10-27 22:15:47 -0300

    Add fecha  Familiar model + Add table+css familia_list

--- ejemplo/models.py
+++ ejemplo/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+import datetime
 
 class Familiar(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200)
     numero_pasaporte = models.IntegerField()
+    fecha = models.DateField(default=datetime.date.today)
     
     def __str__(self):
-        return f"{self.nombre}, {self.numero_pasaporte}, {self.id}"
+        return f"Nombre: {self.nombre}, Passport:{self.numero_pasaporte}, Id:{self.id}, Fecha:{self.fecha}"
 
 class Usuarios(models.Model):
     usuario = models.CharField(max_length = 30)
--- seed_data.py
+++ seed_data.py
@@ -1,8 +1,10 @@
 from ejemplo.models import Familiar
+import datetime
 
-Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
-Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
-Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
-Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
+Familiar(nombre="Rosario",fecha=datetime.date(1980, 10, 19), direccion="Av 9 de Julio 745", numero_pasaporte=123123).save()
+Familiar(nombre="Alberto", fecha=datetime.date(1997, 10, 19),direccion="Belgrano 152", numero_pasaporte=12341234).save()
+Familiar(nombre="Samuel", fecha=datetime.date(2001, 10, 19),direccion="Salta 413", numero_pasaporte=12345123).save()
+Familiar(nombre="Florencia", fecha=datetime.date(2022, 10, 19),direccion="San Martin 745", numero_pasaporte=12345612).save()
+
 
 print("Se cargo con exito los usuarios de pruebas")
