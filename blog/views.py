67ddb60facbfb26f9a68612c2748f855bed9850f
junmamanu <manu.jmgg@gmail.com>
2022-10-29 18:34:37 -0300

    se actualizo todo hasta la clase 22, login, logout y vistas datail, form y list

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,49 @@
 from django.shortcuts import render
-from blog.models import Configuracion
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
+from blog.models import Post
 
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+@login_required
 def index(request):
-    configuracion = Configuracion.objects.first()
-    return render(request,"blog/index.html", {'configuracion': configuracion} )
+    return render(request, 'blog/index.html')
+
+
+class BlogLogin(LoginView):
+    template_name = 'blog/blog_login.html'
+    next_page = reverse_lazy("list-post")
+
+class BlogLogout(LogoutView):
+    template_name = 'blog/blog_logout.html'
+
+
+class ListPost(ListView):
+    model=Post
+
+class CreatePost(CreateView):
+    model=Post
+    fields = ['title', 'short_content', 'content']
+    success_url = reverse_lazy("list-post")
+
+class DetailPost(DetailView):
+    model=Post
+
+class UpdatePost(UpdateView):
+    model=Post
+    fields=['title', 'short_content', 'content']
+    success_url = reverse_lazy("list-post")
+
+class DeletePost(DeleteView):
+    model=Post
+    success_url = reverse_lazy("list-post")
+
+
+class SearchPostByName(ListView):
+    def get_queryset(self):
+        blog_title = self.request.GET.get('post-title')
+        return Post.objects.filter(title__icontains=blog_title)
